57dafdef2b0ae68548a438b28bd6de17b73109e9
Leonardo Renner <leonardorenner96@gmail.com>
2025-02-27 14:43:52 -0300

    subquery com single-row

---
+++ subquery_com_single_row.sql
@@ -0,0 +1,33 @@
+-- Sub-Consultas Single-Row
+
+SELECT first_name, last_name, job_id, salary
+FROM   employees
+WHERE  salary >
+                (SELECT AVG(NVL(salary,0))
+                 FROM employees);              
+
+-- Utilizando Sub-consultas na Cláusula HAVING
+
+SELECT e1.department_id, MAX(e1.salary)
+FROM   employees e1
+GROUP BY e1.department_id
+HAVING MAX(salary) <  (SELECT AVG(e2.salary)
+                       FROM   employees e2);
+                                                  
+-- Erros utilizando Sub-consultas Single-Row
+
+SELECT employee_id, first_name, last_name
+FROM   employees
+WHERE  salary =
+                (SELECT    AVG(NVL(salary,0))
+                 FROM      employees
+                 GROUP BY  department_id);
+
+-- O que ocorre quando a Sub-Consulta retorna nenhuma linha?
+
+SELECT employee_id, first_name, last_name
+FROM   employees
+WHERE  last_name =  (SELECT last_name
+                     FROM   employees
+                     WHERE  last_name = 'Suzuki');
+                     
